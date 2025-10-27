439b17ad09020aede971949db07de1eacd266dec
Anthon Mølgaard Steiness <Anthon@Steiness.info>
2025-10-27 15:04:08 +0100

    Moved Ollama Chat logic to ollama_conn.py. I am thinking I might create my own llama class that can handle holding the writing tasks and have functions to get, set, stop, delete the ongoing tasks.

--- src/bentebot.py
+++ src/bentebot.py
@@ -19,6 +19,12 @@ from src.redis_conn import (
     set_current_model,
     get_current_model
 )
+from src.ollama_conn import (
+    writing,
+    think,
+    chat,
+    generate
+)
 
 
 class bentebot:
@@ -131,8 +137,8 @@ class bentebot:
         # await message.channel.send("You mentioned me?")
         
         r = Response(message)
-        writing = asyncio.create_task(self.writing(r))
-        self.writing_tasks[message.id] = (r, writing)
+        writing_task = asyncio.create_task(writing(r))
+        self.writing_tasks[message.id] = (r, writing_task)
     
     
     
@@ -142,122 +148,8 @@ class bentebot:
         #     await message.channel.send("hello")
         
         r = Response(message)
-        writing = asyncio.create_task(self.writing(r))
-        self.writing_tasks[message.id] = (r, writing)
-    
-    
-    
-        
-    ## TODO: Split the Ollama Logic up into another script
-    async def writing(self, response):
-        full_response = ""
-        try:
-            thinking = asyncio.create_task(self.thinking(response.message))
-            messages = await get_messages(response.message, True)
-            # messages = await self.get_messages(response.message, True)
-            # converted_messages = 
-            
-            async for part in self.chat(messages, context.llama_default_model):
-                if thinking is not None and not thinking.done():
-                    thinking.cancel()
-                    
-                sys.stdout.write(part['message']['content'])
-                sys.stdout.flush()
-                
-                part_content = part['message']['content']
-                full_response += part_content
-                await response.write(part_content, end='...')
-                    
-            await response.write('')
-        except asyncio.CancelledError:
-            await response.message.add_reaction('❌')
-        except Exception as e:
-            await response.message.add_reaction('💩')
-            logging.error("Error answering")
-            logging.error(e)
-            pass
-        finally:
-            if thinking is not None and not thinking.done():
-                thinking.cancel()
-            del self.writing_tasks[response.message.id]  # Remove the task from the dictionary
-             # save bot reply
-            bot_msg = response.r
-            if bot_msg:
-                # await self.save_message_redis(
-                await save_message_redis(
-                    message_id=bot_msg.id,
-                    message_content=full_response,
-                    author=bot_msg.author,
-                    channel_id=bot_msg.channel.id,
-                    attachments=[],
-                    # attachments=bot_msg.attachments,
-                )
-       
-    async def thinking(self, message, timeout=999):
-        try:
-            await message.add_reaction('🤔')
-        except asyncio.CancelledError:
-            pass
-        except Exception as e:
-            logging.error("Error thinking")
-            logging.error(e)
-            await message.add_reaction('💩')
-            pass
-        finally:
-            await message.remove_reaction('🤔', context.discord.user)
-     
-    async def chat(self, messages, model=None, milliseconds=1000):
-        if model is None:
-            model = context.llama_default_model
-        sb = io.StringIO() # create new StringIO object that can write and read from a string buffer
-        t = datetime.datetime.now()
-        try:
-            generator = await context.llama.chat(model, messages=messages, stream=True)
-            async for part in generator:
-                sb.write(part['message']['content']) # write content to StringIO buffer
-                # print(part['message']['content'], end='', flush=True)
-                sys.stdout.write(part['message']['content'])
-                sys.stdout.flush()
-            
-                if milliseconds is None:
-                    # If milliseconds is None, yield every time we get a return from the stream
-                    part['message']['content'] = sb.getvalue()
-                    yield part
-                    sb.seek(0, io.SEEK_SET)
-                    sb.truncate()
-                elif part['done'] or datetime.datetime.now() - t > datetime.timedelta(milliseconds=milliseconds):
-                    part['message']['content'] = sb.getvalue()
-                    yield part
-                    t = datetime.datetime.now()
-                    sb.seek(0, io.SEEK_SET) # change current position in StringIO buffer (io.SEEK_SET = position is relative to beginning of buffer)
-                    sb.truncate() # resizes StringIO buffer to current position. Since current position was just set to 0, this clears the buffer
-                
-        except Exception as e:
-            logging.error("Error getting AI chat response")
-            logging.error(e)
-        
-        
-    async def generate(self, content, model=None):
-        if model is None:
-            model = context.llama_default_model
-        sb = io.StringIO()
-        t = datetime.datetime.now()
-        try:
-            generator = await context.llama.generate(model=model, prompt=content, keep_alive=-1, stream=True)
-            async for part in generator:
-                sb.write(part['response'])
-
-                if part['done'] or datetime.datetime.now() - t > datetime.timedelta(seconds=1):
-                    part['response'] = sb.getvalue()
-                    yield part
-                    t = datetime.datetime.now()
-                    sb.seek(0, io.SEEK_SET)
-                    sb.truncate()
-
-        except Exception as e:
-            logging.error("Error getting AI generate response")
-            logging.error(e)
-        
+        writing_task = asyncio.create_task(writing(r))
+        self.writing_tasks[message.id] = (r, writing_task)
         
     
     ##
---
+++ src/ollama_conn.py
@@ -0,0 +1,115 @@
+import sys, os, io, datetime
+import asyncio
+import logging
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+import context
+from src.redis_conn import (
+    save_message_redis,
+    get_messages
+)
+
+
+async def writing(response):
+    full_response = ""
+    try:
+        thinking = asyncio.create_task(think(response.message))
+        messages = await get_messages(response.message, True)
+        
+        async for part in chat(messages, context.llama_default_model):
+            if thinking is not None and not thinking.done():
+                thinking.cancel()
+                
+            sys.stdout.write(part['message']['content'])
+            sys.stdout.flush()
+            
+            part_content = part['message']['content']
+            full_response += part_content
+            await response.write(part_content, end='...')
+                
+        await response.write('')
+    except asyncio.CancelledError:
+        await response.message.add_reaction('❌')
+    except Exception as e:
+        await response.message.add_reaction('💩')
+        logging.error("Error answering")
+        logging.error(e)
+        pass
+    finally:
+        if thinking is not None and not thinking.done():
+            thinking.cancel()
+        del self.writing_tasks[response.message.id]  # Remove the task from the dictionary
+            # save bot reply
+        bot_msg = response.r
+        if bot_msg:
+            await save_message_redis(
+                message_id=bot_msg.id,
+                message_content=full_response,
+                author=bot_msg.author,
+                channel_id=bot_msg.channel.id,
+                attachments=[],
+                # attachments=bot_msg.attachments,
+            )
+    
+async def think(message, timeout=999):
+    try:
+        await message.add_reaction('🤔')
+    except asyncio.CancelledError:
+        pass
+    except Exception as e:
+        logging.error("Error thinking")
+        logging.error(e)
+        await message.add_reaction('💩')
+        pass
+    finally:
+        await message.remove_reaction('🤔', context.discord.user)
+    
+async def chat(messages, model=None, milliseconds=1000):
+    if model is None:
+        model = context.llama_default_model
+    sb = io.StringIO() # create new StringIO object that can write and read from a string buffer
+    t = datetime.datetime.now()
+    try:
+        generator = await context.llama.chat(model, messages=messages, stream=True)
+        async for part in generator:
+            sb.write(part['message']['content']) # write content to StringIO buffer
+            sys.stdout.write(part['message']['content'])
+            sys.stdout.flush()
+        
+            if milliseconds is None:
+                # If milliseconds is None, yield every time we get a return from the stream
+                part['message']['content'] = sb.getvalue()
+                yield part
+                sb.seek(0, io.SEEK_SET)
+                sb.truncate()
+            elif part['done'] or datetime.datetime.now() - t > datetime.timedelta(milliseconds=milliseconds):
+                part['message']['content'] = sb.getvalue()
+                yield part
+                t = datetime.datetime.now()
+                sb.seek(0, io.SEEK_SET) # change current position in StringIO buffer (io.SEEK_SET = position is relative to beginning of buffer)
+                sb.truncate() # resizes StringIO buffer to current position. Since current position was just set to 0, this clears the buffer
+            
+    except Exception as e:
+        logging.error("Error getting AI chat response")
+        logging.error(e)
+    
+    
+async def generate(content, model=None):
+    if model is None:
+        model = context.llama_default_model
+    sb = io.StringIO()
+    t = datetime.datetime.now()
+    try:
+        generator = await context.llama.generate(model=model, prompt=content, keep_alive=-1, stream=True)
+        async for part in generator:
+            sb.write(part['response'])
+
+            if part['done'] or datetime.datetime.now() - t > datetime.timedelta(seconds=1):
+                part['response'] = sb.getvalue()
+                yield part
+                t = datetime.datetime.now()
+                sb.seek(0, io.SEEK_SET)
+                sb.truncate()
+
+    except Exception as e:
+        logging.error("Error getting AI generate response")
+        logging.error(e)
