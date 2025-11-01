3d1e1c24e241a8c7147688487e3120bf00463900
Anthon Mølgaard Steiness <Anthon@Steiness.info>
2025-11-01 02:06:22 +0100

    Last part given type hints.

--- context.py
+++ context.py
@@ -1,8 +1,11 @@
 ## File for accessing contextual variables from main.py into bentebot.py
+import ollama
+from discord.ext import commands
+import redis as rediss
 
-redis = None
-llama = None
-discord = None
-llama_default_model = None
-super_admin_ids = None
-discord_server_ids = None
+redis: Optional[rediss.Redis] = None
+llama: Optional[ollama.AsyncClient] = None
+discord: Optional[commands.Bot] = None
+llama_default_model: str = None
+super_admin_ids: Optional[str] = None
+discord_server_ids: Optional[str] = None
